88e994fc5e4c758271c379315b8fc06fe2b35de8
brooklyn-p-m <brooklyn.p.merrick@gmail.com>
2026-04-13 10:56:45 -0600

    Finished Reading And writing to Json for Ingredients and Recipes

--- RecipeApp/Classes/App.cs
+++ RecipeApp/Classes/App.cs
@@ -1,9 +1,33 @@
-﻿namespace RecipeApp.Classes
+﻿using System.Text.Json;
+
+namespace RecipeApp.Classes
 {
     public class App
     {
-        public static List<Recipe> Recipes = new List<Recipe>();
+        public static List<Recipe> Recipes = ReadFromRecipes();
 
-        public static Dictionary<Ingredient, int> Ingredients = new Dictionary<Ingredient, int>();
+        public static Dictionary<Ingredient, int> Ingredients = ReadFromIngredients();
+        private static List<Recipe> ReadFromRecipes()
+        {
+            string jsonStringRecipe = File.ReadAllText("recipes.json");
+            List<Recipe> recipes = JsonSerializer.Deserialize<List<Recipe>>(jsonStringRecipe);
+            return recipes;
+        }
+        private static Dictionary<Ingredient, int> ReadFromIngredients()
+        {
+            string jsonStringIngredients = File.ReadAllText("ingredients.json");
+            Dictionary<Ingredient, int> ingredients = JsonSerializer.Deserialize<Dictionary<Ingredient, int>>(jsonStringIngredients);
+            return ingredients;
+        }
+        private static void WriteToJson()
+        {
+            string jsonStringRecipe = JsonSerializer.Serialize(Recipes);
+            File.WriteAllText("recipes.json", jsonStringRecipe);
+
+            string jsonStringIngredients = JsonSerializer.Serialize(Ingredients);
+            File.WriteAllText("ingredients.json", jsonStringIngredients);
+        }
+        
     }
+
 }
